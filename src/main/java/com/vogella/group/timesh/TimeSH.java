a42bff99c249b1ee4715d5d45127191945f8a764
Husam <husamhk1@gmail.com>
2025-10-20 11:52:15 +0200

    new project

---
+++ src/main/java/com/vogella/group/timesh/TimeSH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vogella.group.timesh;
+
+/**
+ *
+ * @author hwssa
+ */
+public class TimeSH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
